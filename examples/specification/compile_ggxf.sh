6b5aa0377058da92494b7dd78abe3e128029b0da
Chris Crook <ccrook@linz.govt.nz>
2022-09-17 08:27:51 +1200

    fix: Retaining real data in full geoid example

--- examples/specification/compile_ggxf.sh
+++ examples/specification/compile_ggxf.sh
@@ -3,5 +3,7 @@ for yaml in *.yaml; do
     ggxf="${yaml%.yaml}.ggxf"
     echo "=================================================================="
     echo "Loading ${yaml}"
-    python3 ../../scripts/ggxf.py -v -y create_dummy_grid_data -n write_cdl_header=true "${yaml}" -o "${ggxf}"
+    dummy="-y create_dummy_grid_data"
+    if [ $yaml = "GGXFspec-E2_grid.yaml" ]; then dummy=''; fi
+    python3 ../../scripts/ggxf.py -v $dummy -n write_cdl_header=true "${yaml}" -o "${ggxf}"
 done
