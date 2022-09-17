9e3bdd37a8d9c3fdcb748a3837237b2814976891
Chris Crook <ccrook@linz.govt.nz>
2022-09-18 10:04:38 +1200

    fix: Correcting the sign of the longitudeOffset in NTv2 file

---
+++ examples/ntv2/ca_ntv2/build_group_yaml.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+python3 ../../../scripts/gdal_to_ggxf_grid_headers.py --parameters="latitudeOffset/longitudeOffset:-1/latitudeOffsetUncertainty/longitudeOffsetUncertainty" NTV2_0.GSB group.yaml
