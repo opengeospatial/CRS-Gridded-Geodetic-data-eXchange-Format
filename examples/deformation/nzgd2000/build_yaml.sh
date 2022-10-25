b3b571af4a0bce37f262a0537fb4133415c7372c
Chris Crook <ccrook@linz.govt.nz>
2022-10-26 08:51:49 +1300

    fix: Removing extentTemporal from NZGD2000 examples

--- examples/deformation/nzgd2000/build_yaml.sh
+++ examples/deformation/nzgd2000/build_yaml.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-python3 ../../../scripts/deformation_model_to_ggxf_yaml.py -m nzgd2000_meta.yaml nzdm/nz_linz_nzgd2000-20180701.json nzgd2000-20180701.yaml
+python3 ../../../scripts/deformation_model_to_ggxf_yaml.py --discard-temporal-extent -m nzgd2000_meta.yaml nzdm/nz_linz_nzgd2000-20180701.json nzgd2000-20180701.yaml
